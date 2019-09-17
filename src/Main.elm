b485023fc2ef66cbd9edaff9212895ea3e4dcbf0
Vincent Cheloudiakoff <vcheloudiakoff@gmail.com>
2019-09-17 15:50:38 +0200

    elm 0.19.1 beta + remove src/Helpers/Main.elm

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,7 @@ module Main exposing (..)
 -- import Galerie.Object.Artwork as Artwork
 
 import Browser
+import Debug exposing (log)
 import Galerie.Object
 import Galerie.Object.Artist as Artist
 import Galerie.Object.Artwork as Artwork
@@ -13,13 +14,27 @@ import Graphql.Operation exposing (RootQuery)
 import Graphql.OptionalArgument exposing (OptionalArgument(..))
 import Graphql.SelectionSet as SelectionSet exposing (SelectionSet)
 import Helpers.Main
-import Html exposing (Html, div, h1, img, text)
-import Html.Attributes exposing (src)
+import Html exposing (Html, a, div, h1, img, input, label, p, pre, text)
+import Html.Attributes exposing (href, src, type_)
+import Html.Events exposing (onClick)
+import PrintAny
+import Regex
 import RemoteData exposing (RemoteData)
 
 
 
 ---- MODEL ----
+
+
+type Msg
+    = ToggleAliases
+    | GotResponse (RemoteData (Graphql.Http.Error Response) Response)
+
+
+type alias Model =
+    { hideAliases : Bool
+    , response : RemoteData (Graphql.Http.Error Response) Response
+    }
 
 
 type alias Response =
@@ -61,18 +76,21 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         GotResponse response ->
-            ( response, Cmd.none )
+            ( { model | response = response }, Cmd.none )
+
+        ToggleAliases ->
+            ( { model | hideAliases = not model.hideAliases }, Cmd.none )
 
 
 makeRequest : Cmd Msg
 makeRequest =
-    query
+    rootQuery
         |> Graphql.Http.queryRequest "http://localhost:3000/graphql"
         |> Graphql.Http.send (RemoteData.fromResult >> GotResponse)
 
 
-query : SelectionSet Response RootQuery
-query =
+rootQuery : SelectionSet Response RootQuery
+rootQuery =
     SelectionSet.map Response
         (Query.artists (\n -> { n | order_by = Present "nickname asc" }) artist)
 
@@ -93,27 +111,68 @@ query =
 ---- PROGRAM ----
 
 
-type Msg
-    = GotResponse Model
-
-
-type alias Model =
-    RemoteData (Graphql.Http.Error Response) Response
-
-
 init : Flags -> ( Model, Cmd Msg )
 init _ =
-    ( RemoteData.Loading, makeRequest )
+    ( { hideAliases = False, response = RemoteData.Loading }, makeRequest )
 
 
 type alias Flags =
     ()
 
 
-main : Helpers.Main.Program Flags Model Msg
 main =
-    Helpers.Main.document
+    Browser.document
         { init = init
         , update = update
-        , queryString = Document.serializeQuery query
+        , subscriptions = \_ -> Sub.none
+        , view = view (Document.serializeQuery rootQuery)
         }
+
+
+view : String -> Model -> Browser.Document Msg
+view query model =
+    { title = "Query Explorer"
+    , body =
+        [ div []
+            [ div []
+                [ h1 [] [ Html.text "Generated Query" ]
+                , p [] [ toggleAliasesCheckbox ]
+                , pre []
+                    [ (if model.hideAliases then
+                        query
+                            |> stripAliases
+
+                       else
+                        query
+                      )
+                        |> Html.text
+                    ]
+                ]
+            , div []
+                [ h1 [] [ Html.text "Response" ]
+                , log "SUBMODEL" model |> PrintAny.view
+                ]
+            ]
+        ]
+    }
+
+
+toggleAliasesCheckbox : Html Msg
+toggleAliasesCheckbox =
+    label []
+        [ input [ type_ "checkbox", onClick ToggleAliases ] []
+        , Html.text " Show Aliases "
+        , a [ href "https://github.com/dillonkearns/elm-graphql/blob/master/FAQ.md#how-do-field-aliases-work-in-dillonkearnselm-graphql" ]
+            [ Html.text "(?)"
+            ]
+        ]
+
+
+stripAliases : String -> String
+stripAliases query =
+    query
+        |> Regex.replace
+            (Regex.fromStringWith { multiline = True, caseInsensitive = True } "^(\\s*)\\w+: "
+                |> Maybe.withDefault Regex.never
+            )
+            (\match -> match.submatches |> List.head |> Maybe.withDefault Nothing |> Maybe.withDefault "")
